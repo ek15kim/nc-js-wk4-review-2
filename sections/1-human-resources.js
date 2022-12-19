247744f9074835d3fdcb39636f0988046565018d
ek15kim <vilche1525@gmail.com>
2022-12-19 15:53:11 +0000

    refactor: section 1 functions in js

---
+++ sections/1-human-resources.js
@@ -0,0 +1,25 @@
+// MAKE IT PURE 
+
+import { StringifyOptions } from "querystring"
+
+const removeAgents = (employees) => employees.filter(employee => employee.profession !== 'mole')
+
+const makeNameTags = (guestList) => {
+    return guestList.map(guest => [guest.title, guest.forename, `${guest.surname},`, guest.company].join(" ")
+    )
+}
+
+const createPoll = (polls) => {
+
+    if (!polls) throw Error("no input!")
+
+    return polls.reduce((acc, cur) => {
+        if (!acc[cur]) {
+            acc[cur] = 0
+        }
+        acc[cur] += 1
+        return acc
+    }, {});
+}
+
+module.exports = { removeAgents, makeNameTags, createPoll }
